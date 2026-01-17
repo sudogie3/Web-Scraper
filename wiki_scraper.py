b5349af8c0580b853146d0e1e0802fa66774e8f1
sudogie3 <szymon@skierski.com>
2026-01-17 21:46:18 +0100

    updated the count_words function and did some changes in other functions

--- wiki_scraper.py
+++ wiki_scraper.py
@@ -1,15 +1,18 @@
 import argparse
 import csv
+import re
+
 import wordfreq
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 
-#Musze wybrać jakiego typu ma być to wiki (kanye west , marvel , ekipa friza)
+# Musze wybrać jakiego typu ma być to wiki (kanye west , marvel , ekipa friza)
 
 class Scraper():
-    instance = None
-    def __init__(self , link_URL ,use_local_html_file_instead=False ):
+
+    def __init__(self , link_URL ,use_local_html_file_instead=False):
         self.link = link_URL
         self.use_lokal = use_local_html_file_instead
 
@@ -25,7 +28,7 @@ class Scraper():
     def SiteDownloader(self, URL):
         response = requests.get(URL)
         if response.status_code != 200:
-            print(f"Nie ma takiej strony: {URL}")
+            print(f"Nie ma takiej strony: {URL} lub nie działa")
             return None
         soup = BeautifulSoup(response.text, 'html.parser')
         return soup
@@ -34,7 +37,8 @@ class Scraper():
         phrase_tmp = self.changingSpaceTo_(phrase)
         URL_tmp = self.link + '/' + phrase_tmp
         soup = self.SiteDownloader(URL_tmp)
-        para  = soup.find('p').text
+        # zrobić to w div class
+        para  = soup.find('p').get_text()
         print(para)
 
 
@@ -51,12 +55,62 @@ class Scraper():
         table = tables[number - 1]
         rows = table.find_all('tr') # wiersze
         data = []
+        print(rows)
+        tableForPandas = []
         for row in rows:
             dataAndHeadersInRow = row.find_all(['td' , 'th']) # dane i nagłówki
             dataAndHeadersInRowText = [data.get_text().strip() for data in dataAndHeadersInRow] # bierzemy jedynie tekst
-                                                                                      # z każdej komorki
+                                                                                     # z każdej komorki
+            print(dataAndHeadersInRow)
+            tableForPandas.append(pd.Series(dataAndHeadersInRowText))
             data.append(dataAndHeadersInRowText)
-        print(data)
+        #print(tableForPandas)
+
+    def count_words(self , phrase):
+        phrase_tmp = self.changingSpaceTo_(phrase)
+        soup = self.SiteDownloader(self.link + '/' + phrase_tmp)
+
+        if soup is None:
+            return
+        text = soup.find("div" , class_ = "mw-content-ltr mw-parser-output").get_text(strip=True , separator=' ')
+
+        print(text)
+        words = []
+        word = ""
+        skips ={"(" , ")" , "[" , "]" , "{" , "}" , "\"" , "\'" , ";" , ":" , "*" , "!" , "?" , "`", "." , "," , "'s"}
+        for letter in text:
+            if letter == " ":
+                words.append(word)
+                word = ""
+            elif letter in skips:
+                pass
+            else:
+                word += letter.lower()
+        for word in text:
+            pass
+        setOfWords = {}
+        for word in words:
+            if word not in setOfWords:
+                setOfWords[word] = 1
+            else:
+                setOfWords[word] += 1
+        df = pd.DataFrame(list(setOfWords.items()) , columns=['słowa' , 'licznik'])
+        print(df.head(n=100))
+
+
+
+
+
+
+
+
+    def analyze_relative_word_freq(self):
+        pass
+
+
+    def auto_count_words(self):
+        pass
+
 
         # with open(file_name , 'w' , encoding='utf-8') as csvfile:
         #     writer = csv.writer(csvfile)
@@ -98,4 +152,5 @@ def creating_parser():
 if __name__ == '__main__':
     URL = 'https://bulbapedia.bulbagarden.net/wiki'
     obiekt = Scraper(URL)
-    obiekt.table('Type' , 2)
+    #obiekt.table('Type' , 2)
+    obiekt.count_words('Type')
